69d884d368c65406c0f44d482f6d533b8b86548c
BackGroundSignal <100814237+BackGroundSignal@users.noreply.github.com>
2026-09-02 12:21:11 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+tasks = []
+done = []
+
+while True:
+    os.system("cls")
+
+    tasks = sorted(tasks)
+    done = sorted(done)
+
+    print(f"tasks: \n")
+    for task in tasks:
+        print(f"[ ] {task}")
+    for task in done:
+        print(f"[X] {task}")
+
+    u_input = input("\nEnter your task: ").strip()
+
+    if u_input in tasks:
+        done.append(u_input)
+        tasks.remove(u_input)
+        continue
+
+    if u_input in done:
+        done.remove(u_input)
+        continue
+
+    tasks.append(u_input)
